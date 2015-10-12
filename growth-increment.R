84597e3c84c1e1cb5690309f19254e9f2c4be77a
kholsinger <keholsinger@gmail.com>
2015-10-11 20:24:12 -0400

    Working version with plotting
    I’m not positive I have the indexing right, but the results seem to
    make sense. I need to check the indexing more carefully to make sure
    I’ve done it correctly.

--- growth-increment.R
+++ growth-increment.R
@@ -6,6 +6,8 @@ library(R2jags)
 
 rm(list=ls())
 
+debug <- FALSE
+
 ## load detr() and helper functions
 source("detr.R")
 source("allfunctionsdavid.v13.R")
@@ -13,30 +15,29 @@ source("allfunctionsdavid.v13.R")
 ## set paths
 plot.data.path <- "plot-data"
 
+## N.B.: VPD series does not include 2014. Requires manual adjustments below
+##
 start.series <- 1981
 end.series <- 2014
-## time period for monthly weather data
 ## final.month: last month of data relevant to current year
 ##   growth increment
-## n.months: number of months included as covariates
 final.month <- 8
-n.months <- 20
+## n.months: number of months included as covariates
+##
+n.months <- 6
 
-debug <- FALSE
-
+## MCMC settings
+##
+n.burnin <- 1000
+n.iter <- 6000
+n.thin <- 1
 if (debug) {
-  n.chains <- 1
-  n.burnin <- 500
-  n.iter <- 1000
-  n.thin <- 1
+  n.chains <- 2
   ## to allow replication of results across runs in JAGS
   ##
   set.seed(1)
 } else {
   n.chains <- 5
-  n.burnin <- 5000
-  n.iter <- 30000
-  n.thin <- 25
 }
 
 ### load data
@@ -54,19 +55,26 @@ for(i in 1:length(files)) {
                      start = as.numeric(rownames(a)[1]),
                      frequency = 1)
   data.tr.det[[i]] <- detr(data.tr[[i]],10)
-  data.tr.det[[i]] <- window(data.tr.det[[i]], start=start.series+1, end=end.series)
+  data.tr.det[[i]] <- window(data.tr.det[[i]],
+                             start=start.series+1,
+                             end=end.series)
 }
 
 ## climate data
 ##
+## end.series re-defined to deal with VPD mismatch
+##
+end.series <- 2013
 prism <- read.table("PRISM_MCN.txt", header = T)      #starts Jan 1981
+prism <- subset(prism, year <= end.series)
 vpd <- ts(read.table("vpd_MCC_cru1901_1913.txt", header = T),
           start = 1901,
           frequency = 12)
-vpd81 <- window(vpd, start = start.series, end = end.series, extend = T)
+vpd81 <- window(vpd, start = start.series, end = end.series+1, extend = TRUE)
 ##
-data.climate <- cbind(prism, c(as.vector(vpd81), rep(NA,11)))
+data.climate <- cbind(prism, head(as.vector(vpd81), -1))
 colnames(data.climate) <- c(colnames(prism), "vpd")
+data.climate <- subset(data.climate, !is.na(vpd))
 rm(prism, vpd, vpd81)
 
 ## translate data into format required for JAGS analysis
@@ -76,7 +84,8 @@ rm(prism, vpd, vpd81)
 ## only for first set of plot data at the moment
 ##
 data <- data.frame(t(data.tr.det[[1]]))
-colnames(data) <- seq(from=start.series+1, to=end.series, by=1)
+colnames(data) <- seq(from=start.series+1, to=end.series+1, by=1)
+data$"2014" <- 2014
 gi.data <- reshape(data,
                    idvar="id",
                    ids=row.names(data),
@@ -85,7 +94,11 @@ gi.data <- reshape(data,
                    varying=list(colnames(data)),
                    direction="long",
                    v.names="gi")
+gi.data <- subset(gi.data, yr <= end.series)
 gi.data$yr <- gi.data$yr - start.series
+## exclude growth increments with estimate=0
+##
+gi.data <- subset(gi.data, gi > 0)
 ##
 ## extract for JAGS
 ##
@@ -95,22 +108,25 @@ indiv <- as.numeric(as.factor(gi.data$id))
 
 ## weather data by year
 ##
-ppt <- matrix(nrow=end.series-start.series, ncol=n.months+1)
-tmn <- matrix(nrow=end.series-start.series, ncol=n.months+1)
-tmx <- matrix(nrow=end.series-start.series, ncol=n.months+1)
-vpd <- matrix(nrow=end.series-start.series, ncol=n.months+1)
+ppt <- matrix(nrow=end.series-start.series, ncol=n.months)
+tmn <- matrix(nrow=end.series-start.series, ncol=n.months)
+tmx <- matrix(nrow=end.series-start.series, ncol=n.months)
+vpd <- matrix(nrow=end.series-start.series, ncol=n.months)
 for (i in (start.series+1):end.series) {
   yr <- i - start.series
-  tmp <- subset(data.climate, year==(i-1) | (year==i & month <= final.month))
-  ppt[yr,1] <- yr
-  tmn[yr,1] <- yr
-  tmx[yr,1] <- yr
-  vpd[yr,1] <- yr
+  if (n.months > final.month) {
+    tmp <- subset(data.climate, (year==(i-1)
+                                 & month > 12 - (n.months-final.month))
+                  | (year==i & month <= final.month))
+  } else {
+    tmp <- subset(data.climate, year==i & month>=final.month - (n.months+1)
+                  & month <= final.month)
+  }
   for (j in 1:n.months) {
-    ppt[yr,j+1] <- tmp$ppt.mm[j]
-    tmn[yr,j+1] <- tmp$TmeanC[j]
-    tmx[yr,j+1] <- tmp$TmaxC[j]
-    vpd[yr,j+1] <- tmp$vpd[j]
+    ppt[yr,j] <- tmp$ppt.mm[j]
+    tmn[yr,j] <- tmp$TmeanC[j]
+    tmx[yr,j] <- tmp$TmaxC[j]
+    vpd[yr,j] <- tmp$vpd[j]
   }
 }
 
@@ -141,8 +157,11 @@ jags.pars <- c("beta.0",
                "beta.tmn",
                "beta.tmx",
                "beta.vpd",
-               "tau.resid",
-               "tau.indiv")
+               "mu.year",
+               "mu.indiv",
+               "mu.year.indiv",
+               "var.resid",
+               "var.indiv")
 fit <- jags(data=jags.data,
             inits=NULL,
             parameters=jags.pars,
@@ -156,5 +175,5 @@ opt.old <- options(width=120)
 print(fit, digits.summary=3)
 options(opt.old)
 
-
-
+save(fit, n.months, gi, year, indiv, start.series, end.series,
+     file="results.Rsave")
---
+++ plot-results.R
@@ -0,0 +1,120 @@
+library(R2jags)
+library(ggplot2)
+library(reshape)
+
+rm(list=ls())
+
+results.file <- "results.Rsave"
+load(file=results.file)
+
+## coefficient plots
+##
+beta.0 <- fit$BUGSoutput$sims.list$beta.0
+beta.ppt <- fit$BUGSoutput$sims.list$beta.ppt
+beta.tmn <- fit$BUGSoutput$sims.list$beta.tmn
+beta.tmx <- fit$BUGSoutput$sims.list$beta.tmx
+beta.vpd <- fit$BUGSoutput$sims.list$beta.vpd
+
+n.reps <- nrow(beta.0)
+
+months <- c("Jan.1", "Feb.1", "Mar.1", "Apr.1", "May.1", "Jun.1",
+            "Jul.1", "Aug.1", "Sep.1", "Oct.1", "Nov.1", "Dec.1",
+            "Jan.2", "Feb.2", "Mar.2", "Apr.2", "May.2", "Jun.2",
+            "Jul.2", "Aug.2")
+months <- months[seq(from=length(months)-(n.months-1), to=length(months))]
+
+par <- character(0)
+month <- character(0)
+values <- numeric(0)
+for (i in 1:length(months)) {
+  par <- c(par, rep("PPT", n.reps))
+  month <- c(month, rep(months[i], n.reps))
+  values <- c(values, beta.ppt[,i])
+
+  par <- c(par, rep("Tmeann", n.reps))
+  month <- c(month, rep(months[i], n.reps))
+  values <- c(values, beta.tmn[,i])
+
+  par <- c(par, rep("Tmax", n.reps))
+  month <- c(month, rep(months[i], n.reps))
+  values <- c(values, beta.tmx[,i])
+
+  par <- c(par, rep("VPD", n.reps))
+  month <- c(month, rep(months[i], n.reps))
+  values <- c(values, beta.vpd[,i])
+}
+
+for.plot <- data.frame(par=par, month=month, values=values)
+for.plot$month <- factor(for.plot$month, levels=months)
+
+p <- ggplot(for.plot, aes(x=month, y=values)) +
+     geom_hline(yintercept=0, linetype="dashed") +
+     geom_boxplot() +
+     facet_grid(par ~ .)
+print(p)
+
+## variation across years
+##
+mu.year <- fit$BUGSoutput$sims.list$mu.year
+n.reps <- nrow(mu.year)
+n.yrs <- ncol(mu.year)
+years <- seq(start.series+1, end.series, by=1)
+year <- numeric(0)
+value <- numeric(0)
+for (i in 1:n.yrs) {
+  year <- c(year, rep(years[i], n.reps))
+  value <- c(value, mu.year[,i])
+}
+for.plot <- data.frame(Year=as.factor(year), Predicted=value)
+
+p <- ggplot(for.plot, aes(x=Year, y=Predicted)) +
+     geom_boxplot()
+print(p)
+
+## variation across individuals
+##
+mu.indiv <- fit$BUGSoutput$sims.list$mu.indiv
+n.reps <- nrow(mu.indiv)
+n.inds <- ncol(mu.indiv)
+indivs <- seq(1, n.inds, by=1)
+indiv <- numeric(0)
+value <- numeric(0)
+for (i in 2:n.inds) {
+  indiv <- c(indiv, rep(i, n.reps))
+  value <- c(value, mu.indiv[,i])
+}
+for.plot <- data.frame(Individual=as.factor(indiv), Predicted=value)
+
+p <- ggplot(for.plot, aes(x=Individual, y=Predicted)) +
+     geom_boxplot()
+print(p)
+
+## residual plots (using posterior mean)
+##
+## reload data since year gets used above
+##
+load(file=results.file)
+mu.year.indiv <- fit$BUGSoutput$mean$mu.year.indiv
+n.obs <- length(gi)
+Observed <- gi
+Predicted <- numeric(n.obs)
+Residual <- numeric(n.obs)
+for (i in 1:n.obs) {
+  Predicted[i] <- mu.year.indiv[year[i], indiv[i]]
+}
+Residual <- Observed - Predicted
+
+for.plot <- data.frame(Observed=Observed, Predicted=Predicted,
+                       Residual=Residual)
+for.plot <- subset(for.plot, !is.na(Observed))
+
+p <- ggplot(for.plot, aes(x=Observed, y=Predicted)) +
+     geom_abline(intercept=0, slope=1, linetype="dashed") +
+     geom_point()
+print(p)
+
+p <- ggplot(for.plot, aes(x=Predicted, y=Residual)) +
+     geom_hline(yintercept=0, linetype="dashed") +
+     geom_point()
+print(p)
+
