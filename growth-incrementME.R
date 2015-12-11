4ee51dd022d09c981efcd67d41424a90a4ca7318
mekevans <margaret.ekevans@gmail.com>
2015-12-11 10:43:36 -0700

    Margaret adding a few notes in the code analyzing increment data
    Nothing to be concerned about...me trying to understand every single
    line...

---
+++ growth-incrementME.R
@@ -0,0 +1,89 @@
+library(reshape)
+library(R2jags)
+
+rm(list=ls())
+
+debug <- FALSE
+
+## MCMC settings
+##
+n.burnin <- 1250
+n.iter <- 2500
+n.thin <- 1
+n.chains <- 4
+if (debug) {
+  n.chains <- 2
+  ## to allow replication of results across runs in JAGS
+  ##
+  set.seed(1)
+}
+
+## select last k rows from x
+##
+select.rows <- function(x, k) {
+  y <- x[seq(nrow(ppt)-(k-1),nrow(ppt)),]
+  return(y)
+}
+
+source("prepare-data.R")
+
+## standardize response variable and covariates before JAGS analysis
+##
+gi <- standardize(gi)
+ppt <- standardize(ppt)
+tmn <- standardize(tmn)
+
+## calculate means for last decade for index calculation
+## ME: trying to figure out why this is necessary...
+##
+ppt.mean <- apply(select.rows(ppt, 10), 2, mean)
+tmn.mean <- apply(select.rows(tmn, 10), 2, mean)
+
+## prior for precision gon regression coefficients
+##
+tau.beta <- 1.0
+
+## d.f. for student t in robust regression
+##
+nu <- 2
+
+jags.data <- c("gi",
+               "year",
+               "indiv",
+               "site",
+               "ppt",
+               "tmn",
+               "ppt.mean",
+               "tmn.mean",
+               "n.obs",
+               "n.years",
+               "n.indiv",
+               "n.sites",
+               "n.months",
+               "tau.beta",
+               "nu")
+jags.pars <- c("beta.0",
+               "beta.ppt",
+               "beta.tmn",
+               "mu.year",
+               "mu.indiv",
+               "mu.site",
+               "var.resid",
+               "var.indiv",
+               "var.site",
+               "idx.site")
+fit <- jags(data=jags.data,
+            inits=NULL,
+            parameters=jags.pars,
+            model.file="growth-increment.jags",
+            n.chains=n.chains,
+            n.burnin=n.burnin,
+            n.iter=n.iter,
+            n.thin=n.thin,
+            working.directory=".")
+opt.old <- options(width=120)
+print(fit, digits.summary=3)
+options(opt.old)
+
+save(fit, n.months, gi, year, indiv, site, start.series, end.series,
+     file="resultsME.Rsave")
---
+++ prepare-dataME.R
@@ -0,0 +1,155 @@
+library(dplR)
+library(bootRes)
+library(pspline)
+
+## load detr() and helper functions
+source("detr.R")
+source("allfunctionsdavid.v13.R")
+
+## N.B.: VPD series does not include 2014. Requires manual adjustments below
+##
+start.series <- 1981
+end.series <- 2014
+## final.month: last month of data relevant to current year
+##   growth increment
+final.month <- 8
+## n.months: number of months included as covariates
+##
+n.months <- 20
+
+## set data path
+plot.data.path <- "C:/Users/mekevans/Documents/CDrive/Bayes/DemogRangeMod/ProofOfConcept/treerings/FalkHolsingerProject/MCN2015Data/MCNIncrements/plots"
+
+month.no <- function(year, month, start.year) {
+  num <- (year-start.year)*12 + month
+  return(num)
+}
+
+standardize.vector <- function(x) {
+  x.bar <- mean(x, na.rm=TRUE)
+  s.d. <- sd(x, na.rm=TRUE)
+  return((x-x.bar)/s.d.)
+}
+
+standardize <- function(x) {
+  if (is.matrix(x)) {
+    y <- matrix(nrow=nrow(x),ncol=ncol(x))
+    for (i in 1:ncol(x)) {
+      y[,i] <- standardize.vector(x[,i])
+    }
+  } else {
+    y <- standardize.vector(x)
+  }
+  return(y)
+}
+
+### load data
+##
+## tree-ring data + detrend
+##
+files <- list.files(path=plot.data.path, pattern = "rwl")
+data.tr <- vector(mode = "list", length = length(files))
+data.tr.det <- vector(mode = "list", length = length(files))
+##
+for(i in 1:length(files)) {
+  filename <- paste(plot.data.path, "/", files[i], sep="")
+  a <- read.rwl(filename, header = FALSE)
+  data.tr[[i]] <- ts(a[,1:ncol(a)],
+                     start = as.numeric(rownames(a)[1]),
+                     frequency = 1)
+  data.tr.det[[i]] <- detr(data.tr[[i]],10)
+  data.tr.det[[i]] <- window(data.tr.det[[i]],
+                             start=start.series+1,  # why does this start with 1982?
+                             end=end.series)
+}
+
+## climate data
+setwd("C:/Users/mekevans/Documents/CDrive/Bayes/DemogRangeMod/ProofOfConcept/treerings/FalkHolsingerProject/extractclimate")
+##
+## end.series re-defined to deal with VPD mismatch
+##
+end.series <- 2013
+prism <- read.table("PRISM_MCN.txt", header = T)      #starts Jan 1981
+prism <- subset(prism, year <= end.series)   #chop off the last year of PRISM data (2014) because vpd data end in 2013
+vpd <- ts(read.table("vpd_MCC_cru1901_1913.txt", header = T),
+          start = 1901,
+          frequency = 12)
+vpd81 <- window(vpd, start = start.series, end = end.series+1, extend = TRUE)
+data.climate <- cbind(prism, head(as.vector(vpd81), -1))
+colnames(data.climate) <- c(colnames(prism), "vpd")
+data.climate <- subset(data.climate, !is.na(vpd))
+rm(prism, vpd, vpd81)
+
+## translate data into format required for JAGS analysis
+##
+data <- data.frame(t(data.tr.det[[1]]), site=files[1])
+for (i in 2:length(data.tr.det)) {
+  tmp.data <- data.frame(t(data.tr.det[[i]]), site=files[i])
+  data <- rbind(data, tmp.data)
+}
+colnames(data) <- c(seq(from=start.series+1, to=end.series+1, by=1), "site") #1982-2014, site
+data$"2014" <- 2014 # erase the data in the year 2014 because vpd data ends in 2013
+data$id <- rownames(data)
+years <- setdiff(colnames(data), c("site","id")) # chop off site and id columns, use the rest to make a year label vector
+# reshape the data to make each year*tree's growth increment a row
+gi.data <- reshape(data,
+                   varying=list(years),
+                   v.names="gi",
+                   timevar="yr",
+                   idvar=c("id","site"),
+                   ids=id,
+                   times=as.numeric(years),
+                   direction="long")
+gi.data <- subset(gi.data, yr <= end.series) # chops off 2014 data
+gi.data$yr <- gi.data$yr - start.series
+## exclude growth increments with estimate <= 0
+##
+gi.data <- subset(gi.data, !is.na(gi)) # only removes missing data, not zeros
+##
+## extract for JAGS
+##
+gi <- gi.data$gi
+year <- gi.data$yr
+indiv <- as.numeric(as.factor(gi.data$id))
+
+## weather data by year
+## each row in the following matrices is a year, 32 total 1982-2013, numeric labels 1:32
+## each column is climate data for one month in the 20-month window
+ppt <- matrix(nrow=end.series-start.series, ncol=n.months)
+tmn <- matrix(nrow=end.series-start.series, ncol=n.months)
+tmx <- matrix(nrow=end.series-start.series, ncol=n.months)
+vpd <- matrix(nrow=end.series-start.series, ncol=n.months)
+for (i in (start.series+1):end.series) {
+  yr <- i - start.series
+  ##
+  month.end <- month.no(i, final.month+1, start.series)
+  for (j in 1:n.months) {
+    month <- month.end-j
+    if (debug) {
+      cat(i, j, month, "\n")
+    }
+    ppt[yr,j] <- data.climate$ppt.mm[month]
+    tmn[yr,j] <- data.climate$TmeanC[month]
+    tmx[yr,j] <- data.climate$TmaxC[month]
+    vpd[yr,j] <- data.climate$vpd[month]
+  }
+}
+if (debug) {
+  if (n.months < 9) {
+    months <- c("Jan.1", "Feb.1", "Mar.1", "Apr.1", "May.1", "Jun.1",
+                "Jul.1", "Aug.1", "Sep.1", "Oct.1", "Nov.1", "Dec.1",
+                "Jan.2", "Feb.2", "Mar.2", "Apr.2", "May.2", "Jun.2",
+                "Jul.2", "Aug.2")
+    months <- months[seq(from=length(months), to=length(months)-(n.months-1))]
+
+    rownames(ppt) <- seq(start.series+1, end.series, by=1)
+    colnames(ppt) <- months
+    cat(ppt["1988",],"\n")
+    cat(data.climate$ppt.mm[data.climate$year==1988][final.month:(final.month+1-6)], "\n")
+    cat(ppt["1998",],"\n")
+    cat(data.climate$ppt.mm[data.climate$year==1998][final.month:(final.month+1-6)], "\n")
+    cat(ppt["2008",],"\n")
+    cat(data.climate$ppt.mm[data.climate$year==2008][final.month:(final.month+1-6)], "\n")
+  }
+}
+
