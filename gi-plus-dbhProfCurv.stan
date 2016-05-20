fc6e0b7c4cdd8c65794d0d3920157656196d7d64
mekevans <margaret.ekevans@gmail.com>
2016-05-19 22:09:47 -0400

    added profile curvature to model, started defining seasonal climate variables
    Added profile curvature (not significant). Started a version of
    gi-plus-dbh.R with seasonal climate variables.

---
+++ gi-plus-dbhProfCurv.stan
@@ -0,0 +1,231 @@
+data {
+  // for growth increment component
+  //
+  int<lower=0> n_sites_gi;
+  int<lower=0> n_years;
+  int<lower=0> n_indiv;
+  int<lower=0> n_months;
+  matrix[n_years,n_months] ppt;
+  matrix[n_years,n_months] tmn;
+  matrix[n_indiv,n_years] gi;
+  int<lower=0> site_gi[n_indiv];
+  vector[n_indiv] basal_area_gi;
+  vector[n_indiv] tree_size_gi;
+  vector[n_indiv] height_ratio_gi;
+  vector[n_indiv] radiation_gi;
+  vector[n_indiv] slope_gi;
+  vector[n_indiv] aspect_gi;
+  vector[n_indiv] twi_gi;
+  vector[n_indiv] profcurv_gi;
+
+  // for dbh component
+  //
+  int<lower=0> n_sites_dbh;
+  int<lower=0> n_obs;
+  int<lower=0> n_species;
+  vector[n_obs] dbh_inc;
+  vector[n_obs] tree_size;
+  vector[n_obs] basal_area;
+  vector[n_obs] height_ratio;
+  vector[n_obs] radiation;
+  vector[n_obs] slope;
+  vector[n_obs] aspect;
+  vector[n_obs] twi;
+  vector[n_obs] profcurv;
+  int<lower=0> site_dbh[n_obs];
+  int<lower=0> species[n_obs];
+}
+parameters {
+  // for growth increment component
+  //
+  real beta_0_gi;
+  vector[n_months] beta_ppt;
+  vector[n_months] beta_tmn;
+  vector[n_indiv] mu_indiv;
+  vector[n_sites_gi] mu_site;
+  real<lower=0> sigma_indiv;
+  real<lower=0> sigma_site_gi;
+  real<lower=0> eta_sq;
+  real<lower=0> inv_rho_sq;
+  real<lower=0> sigma_sq;
+
+  // for dbh component
+  //
+  real beta_0_dbh;
+  real beta_size;
+  real beta_size_squared;
+  real beta_basal_area;
+  real beta_height_ratio;
+  real<lower=0> sigma_resid;
+  real<lower=0> sigma_site_dbh;
+  real<lower=0> sigma_species;
+  vector[n_sites_dbh] eps_site;
+  vector[n_species] eps_species;
+
+  // for the connection
+  //
+  real<lower=0> alpha;
+  real gamma_radiation_dbh;
+  real gamma_slope_dbh;
+  real gamma_aspect_dbh;
+  real gamma_twi_dbh;
+  real gamma_profcurv_dbh;
+}
+transformed parameters {
+  // for growth increment component of the model
+  //
+  matrix[n_indiv,n_years] mu_year_indiv;
+  vector[n_years] mu_year;
+  real<lower=0> rho_sq;
+  cov_matrix[n_years] Sigma;
+  // for dbh component of the model
+  //
+  vector[n_obs] mu_indiv_dbh;
+  vector[n_obs] mu_dbh_inc;
+  vector[n_obs] alpha_indiv;
+  // for shared component of the model, scaled by alpha from dbh component
+  //
+  real beta_size_gi;
+  real beta_size_gi_squared;
+  real beta_basal_area_gi;
+  real beta_height_ratio_gi;
+  real gamma_radiation_gi;
+  real gamma_slope_gi;
+  real gamma_aspect_gi;
+  real gamma_twi_gi;
+  real gamma_profcurv_gi;
+
+  // for growth increment component of the model
+  //
+  rho_sq <- inv(inv_rho_sq);
+  // beta_0_gi incorporated into intercept for mu_year_indiv through mu_indiv
+  //
+  mu_year <- ppt*beta_ppt + tmn*beta_tmn;
+  // expectation for individual j in year i is sum of year
+  // and indivdidual effects
+  //
+  for (i in 1:n_indiv) {
+    for (j in 1:n_years) {
+      mu_year_indiv[i,j] <- mu_indiv[i] + mu_year[j];
+     }
+  }
+  // covariance matrix for Gaussian process
+  //
+  for (i in 1:(n_years-1)) {
+    for (j in (i+1):n_years) {
+      Sigma[i,j] <- eta_sq*exp(-rho_sq*pow(i-j,2));
+      Sigma[j,i] <- Sigma[i,j];
+    }
+  }
+  for (i in 1:n_years) {
+    Sigma[i,i] <- eta_sq + sigma_sq;
+  }
+
+  // for dbh component of the model
+  //
+  for (i in 1:n_obs) {
+    mu_dbh_inc[i] <- beta_size*tree_size[i]
+                     + beta_size_squared*pow(tree_size[i], 2.0)
+                     + beta_basal_area*basal_area[i]
+                     + beta_height_ratio*height_ratio[i]
+                     + gamma_radiation_dbh*radiation[i]
+                     + gamma_slope_dbh*slope[i]
+                     + gamma_aspect_dbh*aspect[i]
+                     + gamma_twi_dbh*twi[i]
+                     + gamma_profcurv_dbh*profcurv[i]
+                     + eps_species[species[i]]
+                     + eps_site[site_dbh[i]];
+  }
+
+  // shared component at individual level, alpha scales
+  // regression coefficients from dbh component to gi component
+  //
+  beta_size_gi <- alpha*beta_size;
+  beta_size_gi_squared <- alpha*beta_size_squared;
+  beta_basal_area_gi <- alpha*beta_basal_area;
+  beta_height_ratio_gi <- alpha*beta_height_ratio;
+  gamma_radiation_gi <- alpha*gamma_radiation_dbh;
+  gamma_slope_gi <- alpha*gamma_slope_dbh;
+  gamma_aspect_gi <- alpha*gamma_aspect_dbh;
+  gamma_twi_gi <- alpha*gamma_twi_dbh;
+  gamma_profcurv_gi <- alpha*gamma_profcurv_dbh;
+  for (i in 1:n_indiv) {
+    alpha_indiv[i] <- beta_size_gi*tree_size_gi[i]
+                      + beta_size_gi_squared*pow(tree_size_gi[i], 2.0)
+                      + beta_basal_area_gi*basal_area_gi[i]
+                      + beta_height_ratio_gi*height_ratio_gi[i]
+                      + gamma_radiation_gi*radiation_gi[i]
+                      + gamma_slope_gi*slope_gi[i]
+                      + gamma_aspect_gi*aspect_gi[i]
+                      + gamma_twi_gi*twi_gi[i]
+                      + gamma_profcurv_gi*profcurv_gi[i];
+  }
+}
+model {
+  // priors for growth increment component
+  //
+  beta_0_gi ~ normal(0.0, 1.0);
+  beta_ppt ~ normal(0.0, 1.0);
+  beta_tmn ~ normal(0.0, 1.0);
+  sigma_indiv ~ normal(0.0, 1.0);
+  sigma_site_gi ~ normal(0.0, 1.0);
+  eta_sq ~ normal(0.0, 1.0);
+  inv_rho_sq ~ normal(0.0, 1.0);
+  sigma_sq ~ normal(0.0, 1.0);
+  // priors for dbh component
+  //
+  beta_0_dbh ~ normal(0.0, 1.0);
+  beta_size ~ normal(0.0, 1.0);
+  beta_size_squared ~ normal(0.0, 1.0);
+  beta_basal_area ~ normal(0.0, 1.0);
+  beta_height_ratio ~ normal(0.0, 1.0);
+  sigma_resid ~ normal(0.0, 1.0);
+  sigma_site_dbh ~ normal(0.0, 1.0);
+  sigma_species ~ normal(0.0, 1.0);
+  // prior for shared components
+  //
+  // N.B.: alpha is defined with a lower bound of 0, so it is
+  // effectively a half-normal prior
+  //
+  alpha ~ normal(0.0, sqrt(2.0));
+  gamma_radiation_dbh ~ normal(0.0, 1.0);
+  gamma_slope_dbh ~ normal(0.0, 1.0);
+  gamma_aspect_dbh ~ normal(0.0, 1.0);
+  gamma_twi_dbh ~ normal(0.0, 1.0);
+  gamma_profcurv_dbh ~ normal(0.0, 1.0);
+
+  // likelihood for growth increment component
+  //
+  for (i in 1:n_indiv) {
+    mu_indiv[i] ~ normal(mu_site[site_gi[i]] + alpha_indiv[i], sigma_indiv);
+  }
+  for (i in 1:n_sites_gi) {
+    mu_site[i] ~ normal(beta_0_gi, sigma_site_gi);
+  }
+  // individual site x year combinations
+  //
+  for (i in 1:n_indiv) {
+    gi[i] ~ multi_normal(mu_year_indiv[i], Sigma);
+  }
+
+  // likelihood for dbh component
+  //
+  eps_species ~ normal(0.0, sigma_species);
+  eps_site ~ normal(beta_0_dbh, sigma_site_dbh);
+  dbh_inc ~ normal(mu_dbh_inc, sigma_resid);
+}
+generated quantities {
+  vector[n_indiv] log_lik_gi;
+  vector[n_obs] log_lik_dbh;
+
+  // calculate log likelihoods
+  //
+  for (i in 1:n_indiv) {
+    log_lik_gi[i] <- multi_normal_log(gi[i],
+                                      mu_year_indiv[i],
+                                      Sigma);
+  }
+  for (i in 1:n_obs) {
+    log_lik_dbh[i] <- normal_log(dbh_inc[i], mu_dbh_inc[i], sigma_resid);
+  }
+}
---
+++ gi-plus-dbhSeas.R
@@ -0,0 +1,567 @@
+library(reshape)
+library(rstan)
+
+rm(list=ls())
+
+debug <- FALSE
+compare <- FALSE
+uncoupled <- FALSE
+coupled <- TRUE
+write.results.file <- TRUE
+save <- TRUE
+
+save <- save & !debug
+write.results.file <- write.results.file & !debug
+
+base_year <- 2004 # note that base_year is also defined on line 280
+
+if (coupled) {
+  model.file <- "gi-plus-dbh.stan"
+} else if (uncoupled) {
+  model.file <- "gi-plus-dbh-uncoupled.stan"
+} else {
+  stop("Mis-specification of model")
+}
+
+## MCMC settings
+##
+n.burnin <- 1250
+n.iter <- 2500
+n.thin <- 1
+n.chains <- 4
+if (debug) {
+  n.burnin <- 50
+  n.iter <- 100
+  n.chains <- 2
+}
+
+rstan_options(auto_write = TRUE)
+n.cores <- min(n.chains, parallel::detectCores())
+
+## select last k rows from x
+##
+select.rows <- function(x, k) {
+  y <- x[seq(nrow(ppt)-(k-1),nrow(ppt)),]
+  return(y)
+}
+
+
+is.na.in.row <- function(x) {
+  sum(is.na(x)) > 0
+}
+is.number.in.row <- function(x) {
+  sum(!is.na(x)) > 0
+}
+not.is.na.in.row <- function(x) {
+  !is.na.in.row(x)
+}
+strip.rwl <- function(x) {
+  y <- sub(".rwl", "", x)
+  return(y)
+}
+extract.tree.number <- function(x, site) {
+  y <- sub(site, "",x)
+  y <- sub("[abABN]", "", y, perl=TRUE)
+  y <- as.numeric(substring(y, 1, 4))
+  return(y)
+}
+
+## calculate R^2 (Gelman et al. 2006)
+##
+calc.r2 <- function(fit, y, n.years, n.indiv, n.iter, verbose=FALSE) {
+  mu.year <- extract(fit, pars=c("mu_year"))$mu_year
+  mu.indiv <- extract(fit, pars=c("mu_indiv"))$mu_indiv
+  v.theta <- numeric(n.iter)
+  v.eps <- numeric(n.iter)
+  ## R^2
+  ##
+  for (iter in 1:n.iter) {
+    if (verbose) {
+      if ((iter %% 100) == 0) {
+        cat(".")
+        flush(stdout())
+      }
+      if (iter == n.iter) {
+        cat(iter, "\n")
+        flush(stdout())
+      }
+    }
+    mu.year.indiv <- matrix(nrow=n.indiv, ncol=n.years)
+    mu <- numeric(n.indiv*n.years)
+    eps <- numeric(n.indiv*n.years)
+    for (i in 1:n.indiv) {
+      for (j in 1:n.years) {
+        mu.year.indiv[i,j] <- mu.indiv[iter,i] + mu.year[iter,j]
+      }
+    }
+    for (i in 1:n.indiv) {
+      for (j in 1:n.years) {
+        mu[i] <- mu.year.indiv[i,j]
+        eps[i] <- y[i,j] - mu[i]
+      }
+    }
+    v.theta[iter] <- var(mu+eps)
+    v.eps[iter] <- var(eps)
+  }
+  r2 <- 1 - mean(v.eps)/mean(v.theta)
+  ## lambda
+  ##
+  e.v.eps <- mean(v.eps)
+  for (iter in 1:n.iter) {
+    if (verbose) {
+      if ((iter %% 100) == 0) {
+        cat(".")
+        flush(stdout())
+      }
+      if (iter == n.iter) {
+        cat(iter, "\n")
+        flush(stdout())
+      }
+    }
+    for (i in 1:n.indiv) {
+      for (j in 1:n.years) {
+        mu.year.indiv[i,j] <- mean(mu.indiv[,i] + mu.year[,j])
+      }
+    }
+    for (i in 1:n.indiv) {
+      for (j in 1:n.years) {
+        mu[i] <- mu.year.indiv[i,j]
+        eps[i] <- y[i,j] - mu[i]
+      }
+    }
+  }
+  v.e.eps <- var(eps)
+  lambda <- 1 - v.e.eps/e.v.eps
+  return(list(r2=r2, lambda=lambda))
+}
+
+to.basal.area <- function(dbh) {
+  area <- pi*((dbh/2.0)^2)
+  return(area)
+}
+
+initial.basal.area <- function(obs, gi, base) {## obs is T1_DBH, gi.raw are ring widths, base is the T1 year
+  ## calculating BA backwards from year T1 (2004) to start.series+1 (1982)
+  ## initial basal area thus refers to 1981 basal area
+  n.trees <- nrow(gi)
+  pred <- numeric(n.trees)
+  for (i in 1:n.trees) {
+    dbh <- obs[i] ## each tree's T1_DBH
+    for (yr in base[i]:(start.series+1)) {## from each tree's T1 year (2004) to 1981+1
+      ## gi is radial growth increment
+      ##
+      dbh <- dbh - 2.0*gi[i, as.character(yr)]*0.1
+    }
+    pred[i] <- to.basal.area(dbh)
+  }
+  return(pred)
+}
+
+check.initial.basal.area <- function(obs, gi, base) {
+  n.trees <- nrow(gi)
+  pred <- numeric(n.trees)
+  pred.dbh <- numeric(n.trees)
+  for (i in 1:n.trees) {
+    dbh <- obs[i]
+    for (yr in base[i]:(start.series+1)) {
+      ## gi is radial growth increment
+      ##
+      dbh <- dbh - 2.0*gi[i, as.character(yr)]*0.1
+    }
+    pred[i] <- to.basal.area(dbh)
+    pred.dbh[i] <- dbh
+  }
+  return(data.frame(initial=pred,
+                    initial.dbh=pred.dbh))
+}
+
+get.size.series <- function(obs, gi.raw, base, start, end) {
+  tree.size <- initial.basal.area(obs, gi.raw, base) ## basal area of each tree in 1981
+  ## gi will contain growth *area* increments as calculated from
+  ## radial growth increments
+  ##
+  gi <- matrix(nrow=nrow(gi.raw), ncol=ncol(gi.raw))
+  colnames(gi) <- colnames(gi.raw)
+  current.size <- matrix(nrow=nrow(gi.raw), ncol=ncol(gi.raw)) ## basal area of each tree in each year
+  colnames(current.size) <- colnames(gi.raw)
+  n.trees <- nrow(gi.raw)
+  for (i in 1:n.trees) {
+    old.size <- tree.size[i] ## basal area in 1981
+    ## convert size (area) to radius
+    ##
+    current.radius <- sqrt(old.size/pi) ## this is 1981 radius
+    for (yr in (start+1):end) { ## from 1982 to 2013
+      current.radius <- current.radius + gi.raw[i, as.character(yr)]*0.1
+      ## to.basal.area takes diameter as argument, not radius
+      ##
+      new.size <- to.basal.area(2.0*current.radius)
+      gi[i, as.character(yr)] <- new.size - old.size ## basal area increment calculation
+      current.size[i, as.character(yr)] <- new.size ## basal area for each tree, each year
+      old.size <- new.size
+    }
+    cat(i, " ", as.character(base[i]), ": ")
+    cat(current.size[i, as.character(base[i])], " ")
+    cat(to.basal.area(obs[i]), "\n")
+    stopifnot(abs(current.size[i, as.character(base[i])]
+                  - to.basal.area(obs[i])) < 1.0e-10)
+  }
+  return(list(gi=gi,
+              current.size=current.size))
+}
+
+basal.area.inc <- function(dbh.1, dbh.2) {
+  initial <- to.basal.area(dbh.1)
+  final <- to.basal.area(dbh.2)
+  inc <- final - initial
+  return(inc)
+}
+
+source("prepare-data.R")
+source("dbh-process-data.R")
+
+## exclude plot 154 for the time being
+##
+dbh <- subset(dbh, plot!="154")
+data <- subset(data, site!="154.rwl")
+
+## exclude single outlier in dbh data
+dbh <- subset(dbh, Tree.number!="691")
+
+## exclude single outlier in gi data
+data <- subset(data, id!="1560562a")
+
+## standardize now so that these covariates match between the gi data
+## and the dbh data
+##
+dbh$Tree.height <- standardize(dbh$Tree.height)
+dbh$T1_BasalArea <- standardize(dbh$T1_BasalArea)
+dbh$height.ratio <- standardize(dbh$height.ratio)
+dbh$total <- standardize(dbh$radiation)
+dbh$Slope <- standardize(dbh$Slope)
+dbh$Aspect <- standardize(dbh$Aspect)
+dbh$TWI <- standardize(dbh$SagaTWI)
+dbh$BA2004 <- standardize(dbh$BA2004)
+## exclude any individuals with NA for row
+##
+data <- data[apply(data, 1, not.is.na.in.row),]
+## merge into single data frame for growth increment analysis,
+## but first have to add plot and Tree.number columns to match
+##
+data$plot <- strip.rwl(data$site)
+data$Tree.number <- 0
+for (i in 1:nrow(data)) {
+  data$Tree.number[i] <- extract.tree.number(data$id[i], data$plot[i])
+}
+before <- nrow(data)
+data <- merge(data, dbh)
+after <- nrow(data)
+## make sure levels of each factor reflect the dropped plot
+##
+dbh <- droplevels(dbh)
+data <- droplevels(data)
+## drop outliers
+##
+dbh <- subset(dbh, !(plot==158 & Tree.number==669))
+data <- subset(data, !(plot==137 & Tree.number==2175))
+
+##
+## observations
+##
+
+## growth increment data
+##
+## exclude 2014 from gi data
+##
+base_year <- dbh$T1year
+gi.years <- years[1:(length(years)-1)]
+gi.raw <- data[,gi.years]
+site_gi <- as.numeric(as.factor(data$plot))
+size.series <- get.size.series(data$T1_DBH, gi.raw, base_year,
+                               start.series, end.series)
+## This funny line is here because we were experimenting with analyses using
+## increments in basal area (size.series$gi) instead of increments in radius.
+## Variation in basal area increments is primarily among trees rather than within
+## trees, and the among tree patterns don't make sense.
+##
+gi <- gi.raw
+basal_area_gi <- data$BA2004
+tree_size_gi <- data$T1_BasalArea
+height_ratio_gi <- data$height.ratio
+radiation_gi <- data$total
+slope_gi <- data$Slope
+aspect_gi <- data$Aspect
+twi_gi <- data$TWI
+n_sites_gi <- length(unique(site_gi))
+
+n.indiv <- nrow(data)
+
+### seasonal climate variables
+ppt.data <- data.frame(ppt)
+tmn.data <- data.frame(tmn)
+ppt.data$AMJ <- ppt.data$X4 + ppt.data$X5 + ppt.data$X6
+ppt.data$SO <- ppt.data$X9 + ppt.data$X10
+tmn.data$AMJ <- tmn.data$X4 + tmn.data$X5 + tmn.data$X6
+tmn.data$SO <- tmn.data$X9 + tmn.data$X10
+
+ppt <- standardize(ppt)
+tmn <- standardize(tmn)
+ppt.AMJ <- standardize(ppt.data$AMJ)
+ppt.SO <- standardize(ppt.data$SO)
+tmn.AMJ <- standardize(tmn.data$AMJ)
+tmn.SO <- standardize(tmn.SO)
+
+## dbh data
+##
+dbh_inc <- standardize(dbh$DBH_inc)
+basal_area <- dbh$BA2004
+tree_size <- dbh$T1_BasalArea
+height_ratio <- dbh$height.ratio
+site_dbh <- as.numeric(as.factor(dbh$plot))
+species <- as.numeric(dbh$Species)
+radiation <- dbh$total
+slope <- dbh$Slope
+aspect <- dbh$Aspect
+twi <- dbh$TWI
+n_sites_dbh <- length(unique(site_dbh))
+n_obs <- nrow(dbh)
+n_species <- length(unique(dbh$Species))
+stopifnot(n_species == max(species))
+
+stan.data <- list(gi=gi,
+                  site_gi=site_gi,
+                  basal_area_gi=basal_area_gi,
+                  tree_size_gi=tree_size_gi,
+                  height_ratio_gi=height_ratio_gi,
+                  radiation_gi=radiation_gi,
+                  slope_gi=slope_gi,
+                  aspect_gi=aspect_gi,
+                  twi_gi=twi_gi,
+                  ppt=ppt,
+                  tmn=tmn,
+                  n_years=n.years,
+                  n_indiv=n.indiv,
+                  n_sites_gi=n_sites_gi,
+                  n_months=n.months,
+                  dbh_inc=dbh_inc,
+                  basal_area=basal_area,
+                  tree_size-tree_size,
+                  height_ratio=height_ratio,
+                  slope=slope,
+                  aspect=aspect,
+                  twi=twi,
+                  site_dbh=site_dbh,
+                  species=species,
+                  n_sites_dbh=n_sites_dbh,
+                  n_obs=n_obs,
+                  n_species=n_species)
+stan.pars <- c("beta_0_gi",
+               "beta_ppt",
+               "beta_tmn",
+               "mu_year",
+               "mu_indiv",
+               "mu_site",
+               "sigma_indiv",
+               "sigma_site_gi",
+               "eta_sq",
+               "rho_sq",
+               "sigma_sq",
+               "beta_0_dbh",
+               "beta_size",
+               "beta_size_squared",
+               "beta_basal_area",
+               "beta_height_ratio",
+               "gamma_radiation_dbh",
+               "gamma_slope_dbh",
+               "gamma_aspect_dbh",
+               "gamma_twi_dbh",
+               "beta_size_gi",
+               "beta_size_gi_squared",
+               "beta_basal_area_gi",
+               "beta_height_ratio_gi",
+               "gamma_radiation_gi",
+               "gamma_slope_gi",
+               "gamma_aspect_gi",
+               "gamma_twi_gi",
+               "sigma_resid",
+               "sigma_site_dbh",
+               "sigma_species",
+               "log_lik_gi",
+               "log_lik_dbh")
+if (coupled) {
+  stan.pars <- c(stan.pars,
+                 "alpha")
+}
+fit <- stan(file=model.file,
+            data=stan.data,
+            pars=stan.pars,
+            iter=n.iter,
+            warmup=n.burnin,
+            thin=n.thin,
+            chains=n.chains,
+            cores=n.cores,
+            adapt.delta=0.95)
+opt.old <- options(width=120)
+if (write.results.file) {
+  sink("results-gi-plus-dbh.txt", append=TRUE, split=TRUE)
+}
+cat("\n\n************************************************************\n",
+    sep="")
+cat(date(), "\n",
+    "With Gaussian process model for individuals\n",
+    sep="")
+if (coupled) {
+  cat("   gi regression proportional to dbh regression\n", sep="")
+} else {
+  cat("   gi and dbh regression uncoupled\n", sep="")
+}
+if (use.detrended) {
+  cat("     using detrended data\n", sep="")
+} else {
+  cat("     using raw data\n", sep="")
+}
+cat(before - after,
+    " individuals discarded from growth increment analysis\n",
+    "because of missing covariate data (plot, radiation, slope,\n",
+    "aspect, twi)\n",
+    sep="")
+print.pars <- c("mu_site",
+                "sigma_indiv",
+                "sigma_site_gi",
+                "eta_sq",
+                "rho_sq",
+                "sigma_sq",
+                "beta_0_dbh",
+                "beta_size",
+                "beta_size_squared",
+                "beta_basal_area",
+                "beta_height_ratio",
+                "gamma_radiation_dbh",
+                "gamma_slope_dbh",
+                "gamma_aspect_dbh",
+                "gamma_twi_dbh",
+                "beta_0_gi",
+                "beta_ppt",
+                "beta_tmn",
+                "beta_size_gi",
+                "beta_size_gi_squared",
+                "beta_basal_area_gi",
+                "beta_height_ratio_gi",
+                "gamma_radiation_gi",
+                "gamma_slope_gi",
+                "gamma_aspect_gi",
+                "gamma_twi_gi",
+                "sigma_resid",
+                "sigma_site_dbh",
+                "sigma_species")
+if (coupled) {
+  print.pars <- c(print.pars,
+                  "alpha")
+}
+print(fit,
+      pars=print.pars,
+      digits_summary=3)
+##
+## R^2
+##
+## Not working for now
+##
+if (0) {
+  r2 <- calc.r2(fit, gi, n.years, n.indiv, n.iter)
+  cat("\n\n",
+      "R^2:    ", round(r2$r2, 3), "\n",
+      "lambda: ", round(r2$lambda, 3), sep="")
+}
+ if (write.results.file) {
+  sink()
+}
+options(opt.old)
+if (save) {
+  if (coupled) {
+    save(fit, data, ppt, tmn, dbh, n.months, start.series, end.series,
+         file="results-gi-plus-dbh.Rsave")
+  } else {
+    save(fit, n.months, gi, year, indiv, site, start.series, end.series,
+         file="results-gi-plus-dbh-uncoupled.Rsave")
+  }
+}
+
+if (compare) {
+  stan.data <- list(n_obs=n_obs,
+                    n_plots=n_sites_dbh,
+                    n_species=n_species,
+                    dbh_inc=dbh_inc,
+                    tree_size=tree_size,
+                    height_ratio=height_ratio,
+                    radiation=radiation,
+                    slope=slope,
+                    aspect=aspect,
+                    twi=twi,
+                    plot=site_dbh,
+                    species=species)
+  stan.par <- c("beta_0",
+                "beta_size",
+                "beta_height_ratio",
+                "gamma_radiation",
+                "gamma_slope",
+                "gamma_aspect",
+                "gamma_twi",
+                "sigma_resid",
+                "sigma_plot",
+                "sigma_species",
+                "eps_plot",
+                "eps_species")
+  fit <- stan(file="dbh.stan",
+              data=stan.data,
+              pars=stan.par,
+              iter=n.iter,
+              warmup=n.burnin,
+              thin=n.thin,
+              chains=n.chains,
+              cores=n.cores)
+  opt.old <- options(width=120)
+  cat("\n\nResults from dbh.stan\n")
+  print(fit, digits_summary=3)
+  options(opt.old)
+
+  stan.data <- list(gi=gi,
+                    site=site_gi,
+                    ppt=ppt,
+                    tmn=tmn,
+                    n_years=n.years,
+                    n_indiv=n.indiv,
+                    n_sites=n.sites,
+                    n_months=n.months)
+  stan.pars <- c("beta_0",
+                 "beta_ppt",
+                 "beta_tmn",
+                 "mu_year",
+                 "mu_indiv",
+                 "sigma_indiv",
+                 "sigma_site",
+                 "eta_sq",
+                 "rho_sq",
+                 "sigma_sq",
+                 "log_lik")
+  fit <- stan(file="growth-increment-with-site.stan",
+              data=stan.data,
+              pars=stan.pars,
+              iter=n.iter,
+              warmup=n.burnin,
+              thin=n.thin,
+              chains=n.chains,
+              cores=n.cores)
+  opt.old <- options(width=120)
+  cat("\n\nResults from growth-increment-with-site.stan\n")
+  print(fit,
+        pars=c("beta_0",
+               "beta_ppt",
+               "beta_tmn",
+               "sigma_indiv",
+               "sigma_site",
+               "eta_sq",
+               "rho_sq",
+               "sigma_sq"),
+        digits_summary=3)
+  options(opt.old)
+}
+
